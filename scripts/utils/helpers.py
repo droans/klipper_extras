77e3695b6ae3ef451d1f79b418031ea7b38b5446
Michael Carroll <xavier2cowboys@gmail.com>
2023-02-13 08:47:00 -0500

    Create helpers to poll Python binary for version

--- scripts/utils/helpers.py
+++ scripts/utils/helpers.py
@@ -1,7 +1,14 @@
 import sys
+import subprocess
 from .enums import PythonVersion
 
-def GetPythonVersion():
+def GetPythonVersion(python_binary=None):
+    if python_binary is None:
+        return _get_script_python_binary()
+    else:
+        return _get_binary_python_version(python_binary)
+
+def _get_script_python_binary():
     v = sys.version
     if v[0] == '2':
         python_version = PythonVersion.PYTHON2
@@ -13,6 +20,18 @@ def GetPythonVersion():
     return python_version
 
 version = GetPythonVersion()
+
+def _get_binary_python_version(python_binary):
+    script_py = _get_script_python_binary()
+    cmd = python_binary
+    cmd += ' -c "import sys; print(sys.version_info.major)"'
+    v = subprocess.check_output(cmd, shell=True)
+    v = int(v)
+    if v == 2:
+        result = PythonVersion.PYTHON2
+    elif v == 3:
+        result = PythonVersion.PYTHON3
+    return result
 
 def Input(text=''):
     if version == PythonVersion.PYTHON2:
